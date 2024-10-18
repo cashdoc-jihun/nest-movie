ecc4af8fcce42d2d0e08459ffdb28713291927ec
cashdoc-jihun <jihun.seo@cashdoc.me>
2024-10-18 09:53:00 +0900

    initial commit

---
+++ src/app/movies/[id]/page.tsx
@@ -0,0 +1,20 @@
+import { Suspense } from "react";
+import MovieInfo from "./_components/movie-info";
+import MovieVideos from "./_components/movie-videos";
+
+export default async function MovieDetail({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
+}
